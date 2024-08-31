e2ce32d8258ea89f1c50022801980d7e0d7c45c6
alihab1 <tmsah6491@gmail.com>
2024-08-31 14:54:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     # Token Bot
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN")
+    TG_BOT_TOKEN = TeleBot("TG_BOT_TOKEN")
     API_ID = int(os.environ.get("API_ID"))
            ID = int(os.environ.get("ID"))
     API_HASH = os.environ.get("API_HASH")
