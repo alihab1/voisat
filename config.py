1f3edfa012174b638f9bf0527f6db3555170f419
alihab1 <tmsah6491@gmail.com>
2024-08-31 14:37:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 class Config(object):
     # Token Bot
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID")
-           ID = int(os.environ.get("ID")
+    API_ID = int(os.environ.get("API_ID"))
+           ID = int(os.environ.get("ID"))
     API_HASH = os.environ.get("API_HASH")
